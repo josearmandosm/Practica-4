df6a969c86b4b691172e1d7ea1b2f5df3b0e1fdc
joses <jose.sm01@hotmail.com>
2024-06-16 22:47:14 -0400

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+# Ejercicio 1: Multiplicar todos los elementos por 3
+# Dada una lista de números, multiplica todos los elementos por 3.
+
+numeros = [1, 2, 3, 4, 5]
+numeros_multiplicados = [x * 3 for x in numeros]
+print(numeros_multiplicados)
